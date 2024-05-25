b177d4536ab8f6a2fa9a3bf5e5b665e32f0d17a1
shernadir <nadirbaig324@gmail.com>
2024-05-25 16:04:04 +0500

    company

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -6,26 +6,10 @@ import Image from "next/image";
 export const Company = ({ lng }) => {
   const { t } = useTranslation(lng);
   return (
-    <div className="relative ">
-      <div className="absolute top-10 left-10">
-        <SvrilIcon />
-      </div>
-      <div className="absolute top-10 right-10">
-        <SvrilIcon />
-      </div>
-      <div className="content w-full md:w-4/5 flex-col md:flex md:flex-row items-center gap-4 m-auto my-8">
-        <div className="w-10/12 m-auto md:w-1/2">
-          <Image
-            src="/images/schoolImage.png"
-            alt="Hero Image"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: "100%", height: "100%" }}
-          />
-        </div>
-        <div className="w-11/12 m-auto md:w-1/2">
-          <p className="mt-5 text-6xl text-dark text-center md:text-start">
+    <div className="content bg-white w-full ">
+      <div className="w-4/5 flex-col md:flex md:flex-row items-center gap-4 m-auto my-8 ">
+        <div className=" md:w-3/5 bg-[#E2E8F0] rounded-2xl pl-20 py-5">
+          <p className="text-6xl text-dark text-center md:text-start">
             {t("GET_IN_TOUCH")}
           </p>
           <p
@@ -35,13 +19,23 @@ export const Company = ({ lng }) => {
           </p>
           <div className="flex justify-center md:justify-start mb-10 md:mb-0">
             <button
-              className={`mt-12 text-base font-medium text-white bg-primary py-3 px-8 rounded-full`}
+              className={`text-base font-medium text-white bg-primary py-3 px-8 rounded-full`}
             >
               {t("GET_STARTED")}
             </button>
           </div>
-          <div className="w-full flex  justify-center md:justify-end">
-            <BookIcon />
+        </div>
+
+        <div className="w-2/5 m-auto md:w-1/2 ">
+          <div className="w-10/12 m-auto md:w-1/2 ">
+            <Image
+              src="/images/man.png"
+              alt="Hero Image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "100%" }}
+            />
           </div>
         </div>
       </div>
