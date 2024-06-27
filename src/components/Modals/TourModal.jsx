4aef9d3815a062e226bf03b219f10789f8adac36
shernadir <nadirbaig324@gmail.com>
2024-06-27 15:36:15 +0500

    translated modal for dashboard

--- src/components/Modals/TourModal.jsx
+++ src/components/Modals/TourModal.jsx
@@ -5,49 +5,51 @@ import { ArrowRightIcon, LinesIcon } from "@/svgs/icons";
 import Button from "@/components/Button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useTranslation } from "@/app/i18n/client";
 
-export const TourModal = ({lng}) => {
+export const TourModal = ({ lng }) => {
+  const { t } = useTranslation(lng);
   const steps = [
     {
       id: "step-1",
       img: "/images/tour/tour-1.png",
-      title: "Help you discover your",
-      description: "Dream Career",
+      title: t("HELP_YOU_DISCOVER"),
+      description: t("DREAM_CAREER"),
     },
     {
       id: "step-2",
       img: "/images/tour/tour-2.png",
-      title: "Make job focused learning ",
-      description: "pathways",
+      title: t("MAKE_JOB_FOCUSED_LEARNING"),
+      description: t("PATHWAYS"),
     },
     {
       id: "step-3",
       img: "/images/tour/tour-3.png",
-      title: "Match you to jobs that fit your",
-      description: "skills",
+      title: t("MATCH_YOU_TO_JOBS_THAT_FIT_YOUR"),
+      description: t("SKILLS"),
     },
     {
       id: "step-4",
       img: "/images/tour/tour-4.png",
-      title: "Are you Ready?!",
+      title: t("ARE_YOU_READY"),
       description: "",
     },
     {
       id: "step-5",
       img: "/images/tour/tour-5.png",
-      title: "Discover Your Dream Career.",
+      title: t("DISCOVER_YOUR_DREAM"),
       description: "",
     },
     {
       id: "step-6",
       img: "/images/tour/tour-6.png",
-      title: "Let’s Get Started!",
+      title: t("LETS_GET_STARTED"),
       description: "",
     },
     {
       id: "step-7",
       img: "/images/tour/tour-7.png",
-      title: "Personalized Gamified",
+      title: t("PERSONALIZED_GAMIFIED"),
       description: "",
     },
     // {
@@ -57,7 +59,7 @@ export const TourModal = ({lng}) => {
     //   description: "",
     // },
   ];
-  console.log('modal lng', lng)
+  console.log("modal lng", lng);
   const { tour, setTour, tourStep, setTourStep } = useModals();
   const router = useRouter();
 
@@ -83,36 +85,10 @@ export const TourModal = ({lng}) => {
       title="Tutorial"
       className="!max-w-[50rem] !w-full shadow-xl"
       footerClass={"hidden"}
-      footer={
-        <Button
-          size="lg"
-          className="!w-fit !px-5 gap-2 !font-medium"
-          variant="primaryContained"
-          onClick={handleNext}
-        >
-          Save let&apos;s go <ArrowRightIcon />
-        </Button>
-      }
     >
       <div className="">
-        {/* <div className="flex justify-between my-4">
-          <Button
-            variant="text"
-            className="gap-2 !text-grey-400"
-            onClick={handlePrev}
-          >
-            <ArrowRightIcon className="transform rotate-180 " /> Back
-          </Button>
-          <span>
-            {tourStep + 1}/{steps.length}
-          </span>
-          <Button variant="text" className="gap-2" onClick={handleNext}>
-            {tourStep < steps.length - 1 ? "Next" : "Finish"}{" "}
-            <ArrowRightIcon className="" />
-          </Button>
-        </div> */}
         <div className="text-center flex w-full">
-          <div className="relative flex items-center gap-4 w-full">
+          <div className="relative flex items-center w-full">
             <div className="flex h-full flex-1 min-h-[30rem] bg-primary ">
               <Image
                 alt={`tour-${steps[tourStep].id}`}
@@ -126,31 +102,39 @@ export const TourModal = ({lng}) => {
             <div className="flex  h-full flex-1 min-h-[30rem] ">
               <LinesIcon className="absolute top-0 right-0" />
 
-              <div className="flex flex-col justify-center p-5 w-full">
+              <div className="flex flex-col justify-center p-5 w-full gap-2  min-h-[31rem]">
                 <p className="mt-0 mb-4 text-6xl text-dark-100 leading-[60px] flex gap-2 flex-col">
                   {steps[tourStep].title}
                   <span className="text-6xl sm:text-7xl text-primary">
                     {steps[tourStep].description}
                   </span>
                 </p>
-                  <div
-                    className="relative cursor-pointer flex w-11/12 flex items-center justify-end"
-                    onClick={handleNext}
-                  >
-                    {tourStep === 0 ? <div><div className="rounded-3xl h-8 flex items-center justify-center bg-dark-700 text-primary w-20 text-sm font-bold mr-10">
-                      Next
+                <div
+                  className={`relative cursor-pointer flex w-full flex items-center ${
+                    tourStep === 0 ? "justify-end" : "justify-center"
+                  }`}
+                  onClick={handleNext}
+                >
+                  {tourStep === 0 ? (
+                    <div>
+                      <div className="rounded-3xl h-8 flex items-center justify-start bg-dark-700 text-primary w-20 text-sm font-bold mr-10 pl-2">
+                        {t("NEXT")}
+                      </div>
+                      <div className="rounded-full w-10 h-10 bg-primary absolute flex items-center justify-center right-[20px] -top-[5px]">
+                        <ArrowRightIcon className="text-white" />
+                      </div>
                     </div>
-                    <div className="rounded-full w-10 h-10 bg-primary absolute flex items-center justify-center right-[20px] -top-[5px]">
-                      <ArrowRightIcon className="text-white" />
-                    </div></div> : <Button
-                    size="lg"
-                    className="!w-fit !px-5 gap-2 !w-full !font-medium"
-                    variant="primaryContained"
-                    onClick={handleNext}
-                  >
-                    Save let&apos;s go <ArrowRightIcon />
-                  </Button>}
-                  </div>
+                  ) : (
+                    <Button
+                      size="lg"
+                      className="!w-[50%] !px-5 gap-2 !w-full !font-medium"
+                      variant="primaryContained"
+                      onClick={handleNext}
+                    >
+                      Save let&apos;s go <ArrowRightIcon />
+                    </Button>
+                  )}
+                </div>
               </div>
             </div>
           </div>
